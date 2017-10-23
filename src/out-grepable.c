1264c27a7adc95fa78a036f719300cbd37f6d82a
Rafael Ortiz <rafael@ortizmail.cc>
2017-10-23 19:21:02 -0400

    Added time stamp to greppable output.
    Added changes from https://hml.io/2016/01/20/masscan-logging-demystified
    to out-grepable.c. Greppable output file will now give a format like
    this:
    
    Timestamp: 1453243603 Host: 192.168.0.1 () Ports: 443/open/tcp////

--- src/out-grepable.c
+++ src/out-grepable.c
@@ -130,12 +130,13 @@ static void
 grepable_out_status(struct Output *out, FILE *fp, time_t timestamp,
     int status, unsigned ip, unsigned ip_proto, unsigned port, unsigned reason, unsigned ttl)
 {
-    UNUSEDPARM(timestamp);
+    // UNUSEDPARM(timestamp);
     UNUSEDPARM(out);
     UNUSEDPARM(reason);
     UNUSEDPARM(ttl);
 
-    fprintf(fp, "Host: %u.%u.%u.%u ()",
+    fprintf(fp, "Timestamp: %lu", timestamp);
+    fprintf(fp, "\tHost: %u.%u.%u.%u ()",
                     (unsigned char)(ip>>24),
                     (unsigned char)(ip>>16),
                     (unsigned char)(ip>> 8),
@@ -167,11 +168,12 @@ grepable_out_banner(struct Output *out, FILE *fp, time_t timestamp,
     char banner_buffer[4096];
 
     UNUSEDPARM(ttl);
-    UNUSEDPARM(timestamp);
+    // UNUSEDPARM(timestamp);
     UNUSEDPARM(out);
     UNUSEDPARM(ip_proto);
     
-    fprintf(fp, "Host: %u.%u.%u.%u ()",
+    fprintf(fp, "Timestamp: %lu", timestamp);
+    fprintf(fp, "\tHost: %u.%u.%u.%u ()",
                     (unsigned char)(ip>>24),
                     (unsigned char)(ip>>16),
                     (unsigned char)(ip>> 8),
